0d6a821552d9e61b8c783664ca63e78831c8ac20
Jinan <jinanging2@gmail.com>
2025-06-25 18:47:49 +0900

    [권진환] - 비밀번호 해싱을 통한 보안 기능 추가 - 무결성 검사 과정 추가 - 회원가입 기능 추가 - 자바 스크립트를 통해 동적기능 추가

--- src/main/java/com/jinanging/memo/user/UserController.java
+++ src/main/java/com/jinanging/memo/user/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// view를 위한 컨트롤러
 @RequestMapping("/user")
 @Controller
 public class UserController {
@@ -17,5 +18,7 @@ public class UserController {
 	public String loginInput() {
 		return "user/login";
 	}
+	
+	
 
 }
---
+++ src/main/java/com/jinanging/memo/user/UserRestController.java
@@ -0,0 +1,50 @@
+package com.jinanging.memo.user;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jinanging.memo.user.service.UserService;
+
+// API 구성을 위한 컨트롤러 
+@RequestMapping("/user")
+@RestController  // @COntroller + @ResponseBody
+public class UserRestController {
+	
+	private final UserService userService;
+	
+	public UserRestController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@PostMapping("/join")
+	public Map<String,String> join(
+			@RequestParam String loginId
+			,@RequestParam String password
+			,@RequestParam String name
+			,@RequestParam String email){
+		
+		Map<String,String> resultMap = new HashMap<>();
+		
+		if(userService.addUser(loginId, password, name, email)) {
+			
+			resultMap.put("result", "success");
+			
+			
+		} else {
+			resultMap.put("result", "fail");
+			
+		}
+		
+		
+		
+		
+		return resultMap;
+	}
+	
+
+}
